cfafc107f2657fd5f34ad999b12059712036b8e3
Gullo <lg.laetitiagullo@gmail.com>
2021-02-05 15:05:48 +0100

    math ok

--- 02-maths/04-sort-numbers/script.js
+++ 02-maths/04-sort-numbers/script.js
@@ -15,7 +15,8 @@
 
     document.getElementById("run").addEventListener("click", function() {
 
-        // your code here
+        var num = [2, 4, 14, 10, 90, 23, 16];
+        alert(num.sort(function(a, b){return a-b}));
 
     });
 
