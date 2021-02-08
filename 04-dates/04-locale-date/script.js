347da396a4e94120c52cf130fd2fc9853009d2c6
Gullo <lg.laetitiagullo@gmail.com>
2021-02-08 12:35:21 +0100

    ex dates-04 +/-

--- 04-dates/04-locale-date/script.js
+++ 04-dates/04-locale-date/script.js
@@ -11,8 +11,6 @@
 
 (function() {
 
-    // to change the content of a tag: document.getElementById("element-id").innerHTML = "new-value"
-
-    // your code here
-
+    var d = new Date();
+    document.getElementById("target").innerHTML = d;
 })();
