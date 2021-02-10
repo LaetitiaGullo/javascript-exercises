474fe7cf1495cce4c8565dbfb18e1298974e5cb3
Gullo <lg.laetitiagullo@gmail.com>
2021-02-10 11:11:27 +0100

    ex arrays-04

--- 05-arrays/04-walk-two/script.js
+++ 05-arrays/04-walk-two/script.js
@@ -26,6 +26,12 @@
         "cerise",
     ];
 
-    // your code here
+    document.getElementById("run").addEventListener("click", function() {
+
+        fruits.forEach(fruit => console.log(fruit));
+
+    });
+
+
 
 })();
