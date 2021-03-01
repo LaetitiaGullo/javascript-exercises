534ecf8f7eb4d6c4cc73962cb231435308386960
Gullo <lg.laetitiagullo@gmail.com>
2021-03-01 10:59:45 +0100

    10-asynchronous

--- 10-asynchronous/05-promise-get-comments/script.js
+++ 10-asynchronous/05-promise-get-comments/script.js
@@ -10,5 +10,17 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+    document.querySelector("#run").addEventListener("click", () => {
+        window.lib.getPosts().then((posts) => {
+            Promise.all(
+                posts.map(post=>window.lib.getComments(post.id))
+
+            ).then(()=> {
+                postsComments.forEach((comments, i)=> {
+                    posts[i].comments = comments;
+                });
+                console.log(posts);
+            })
+        })
+    })
 })();
